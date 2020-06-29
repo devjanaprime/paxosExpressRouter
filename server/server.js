e87f29e8d2d332dcb19bd93bf709ca8b2e1eec2a
Dev Jana <devjana@Devs-MBP-2.domain>
2020-06-29 13:51:29 -0500

    basic server up

---
+++ server/server.js
@@ -0,0 +1,13 @@
+// requires
+const express = require( 'express' );
+app = express();
+const bodyParser = require( 'body-parser' );
+// uses
+app.use( express.static( 'server/public' ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+// globals
+const port = 5000;
+// spin up server
+app.listen( port, ()=>{
+    console.log( 'server up:', port );
+})
